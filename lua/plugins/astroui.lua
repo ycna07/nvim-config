420593e6aa0d25a7c0a4ec2c2a5fc409720d0d2d
ycna07 <ycna07@foxmail.com>
2026-07-21 16:38:16 +0800

    fold

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,6 +8,13 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
+
+    folding = {
+      -- whether a buffer should have folding can be true/false for global enable/disable or fun(bufnr:integer):boolean
+      enabled = function(bufnr) return require("astrocore.buffer").is_valid(bufnr) end,
+      -- a priority list of fold methods to try using, available methods are "lsp", "treesitter", and "indent"
+      methods = { "lsp", "treesitter", "indent" },
+    },
     -- change colorscheme
     colorscheme = "astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
