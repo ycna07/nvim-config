af8812e2ffac1fab4a8283d053ee1de9d6ae7f2f
ycna07 <ycna07@foxmail.com>
2026-07-27 14:54:27 +0800

    use catppuccin-latte as defaut colorschme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -16,7 +16,7 @@ return {
       methods = { "lsp", "treesitter", "indent" },
     },
     -- change colorscheme
-    colorscheme = "astrodark",
+    colorscheme = "catppuccin-latte",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
