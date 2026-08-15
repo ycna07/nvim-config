fe4bd1e575fcbd72f2ec92739e17947ab6d3e6f0
ycna07 <ycna07@foxmail.com>
2026-08-15 15:16:24 +0800

    add H L keymap for neo-tree

--- lua/plugins/fm.lua
+++ lua/plugins/fm.lua
@@ -9,8 +9,44 @@ return {
     "MunifTanjim/nui.nvim",
   },
   opts = {
+    commands = {
+
+      parent_or_close = function(state)
+        local node = state.tree:get_node()
+        if node:has_children() and node:is_expanded() then
+          state.commands.toggle_node(state)
+        else
+          require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+        end
+      end,
+      child_or_open = function(state)
+        local node = state.tree:get_node()
+        if node:has_children() then
+          if not node:is_expanded() then -- if unexpanded, expand
+            state.commands.toggle_node(state)
+          else -- if expanded and has children, select the next child
+            if node.type == "file" then
+              state.commands.open(state)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+            end
+          end
+        else -- if has no children
+          state.commands.open(state)
+        end
+      end,
+    },
     window = {
       position = "float",
+      mappings = {
+        h = "parent_or_close",
+        l = "child_or_open",
+      },
+    },
+    filesystem = {
+      follow_current_file = { enabled = true },
+      hijack_netrw_behavior = "open_current",
+      use_libuv_file_watcher = vim.fn.has("win32") ~= 1,
     },
   },
   -- config = function()
