632b0f81e30b845a7c6a677f3046a4c7711e12e6
ycna07 <ycna07@foxmail.com>
2026-09-10 21:45:01 +0800

    feat(misc): add mini.hipatterns highlighters, drop todo-comments

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -135,9 +135,50 @@ return {
     end,
   },
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+    "nvim-mini/mini.hipatterns",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local hipatterns = require("mini.hipatterns")
+      local highlighters = {}
+      local groups = {
+        Fixme = {
+          "fix",
+          "fixme",
+          "xxx",
+          "FIXME",
+        },
+        Hack = {
+          "warn",
+          "hack",
+          "warning",
+        },
+        Note = {
+          "note",
+        },
+        Todo = {
+          "todo",
+        },
+      }
+      for name, words in pairs(groups) do
+        local hl = "MiniHipatterns" .. name
+        for _, word in ipairs(words) do
+          highlighters["tokens_" .. name .. "_" .. word .. "upper"] = {
+            pattern = "%f[%w]" .. word:upper() .. ":+",
+            group = hl,
+          }
+          highlighters["tokens_" .. name .. "_" .. word .. "lower"] = {
+            pattern = "%f[%w]" .. word:lower() .. ":+",
+            group = hl,
+          }
+        end
+      end
+      hipatterns.setup({
+        highlighters = vim.tbl_extend("force", highlighters, {
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        }),
+      })
+    end,
   },
   --lint
   {
